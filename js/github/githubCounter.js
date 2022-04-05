25914f8243808798c856182723601329f5961498
sumithemmadi <sumithemmadi244@gmail.com>
2022-04-05 20:20:13 +0530

    Updated

---
+++ js/github/githubCounter.js
@@ -0,0 +1,67 @@
+var XMLHttpRequest = require('xhr2')
+var http = new XMLHttpRequest();
+let total_stars = 0;
+
+function fromNow(date) {
+    const SECOND = 1000;
+    const MINUTE = 60 * SECOND;
+    const HOUR = 60 * MINUTE;
+    const DAY = 24 * HOUR;
+    const WEEK = 7 * DAY;
+    const MONTH = 30 * DAY;
+    const YEAR = 365 * DAY;
+    const units = [
+        { max: 30 * SECOND, divisor: 1, past1: 'just now', pastN: 'just now', future1: 'just now', futureN: 'just now' },
+        { max: MINUTE, divisor: SECOND, past1: 'a second ago', pastN: '# seconds ago', future1: 'in a second', futureN: 'in # seconds' },
+        { max: HOUR, divisor: MINUTE, past1: 'a minute ago', pastN: '# minutes ago', future1: 'in a minute', futureN: 'in # minutes' },
+        { max: DAY, divisor: HOUR, past1: 'an hour ago', pastN: '# hours ago', future1: 'in an hour', futureN: 'in # hours' },
+        { max: WEEK, divisor: DAY, past1: 'yesterday', pastN: '# days ago', future1: 'tomorrow', futureN: 'in # days' },
+        { max: 4 * WEEK, divisor: WEEK, past1: 'last week', pastN: '# weeks ago', future1: 'in a week', futureN: 'in # weeks' },
+        { max: YEAR, divisor: MONTH, past1: 'last month', pastN: '# months ago', future1: 'in a month', futureN: 'in # months' },
+        { max: 100 * YEAR, divisor: YEAR, past1: 'last year', pastN: '# years ago', future1: 'in a year', futureN: 'in # years' },
+        { max: 1000 * YEAR, divisor: 100 * YEAR, past1: 'last century', pastN: '# centuries ago', future1: 'in a century', futureN: 'in # centuries' },
+        { max: Infinity, divisor: 1000 * YEAR, past1: 'last millennium', pastN: '# millennia ago', future1: 'in a millennium', futureN: 'in # millennia' },
+    ];
+    const diff = Date.now() - (typeof date === 'object' ? date : new Date(date)).getTime();
+    const diffAbs = Math.abs(diff);
+    for (const unit of units) {
+        if (diffAbs < unit.max) {
+            const isFuture = diff < 0;
+            const x = Math.round(Math.abs(diff) / unit.divisor);
+            if (x <= 1) return isFuture ? unit.future1 : unit.past1;
+            return (isFuture ? unit.futureN : unit.pastN).replace('#', x);
+        }
+    }
+};
+
+http.onreadystatechange = function () {
+    if (this.readyState != 4) return;
+
+    if (this.status == 200) {
+        var data2 = JSON.parse(this.responseText);
+        for (let i = 0; i < data2.length; i++) {
+            total_stars += data2[i].stargazers_count
+            if (data2[i].name == "truecallerjs") {
+                document.getElementById('truecallerjs-stars').innerHTML = data2[i].stargazers_count;
+                document.getElementById('truecallerjs-forks').innerHTML = data2[i].forks;
+                document.getElementById('truecallerjs-updated-time').innerHTML = fromNow(data2[i].updated_at);
+            }
+            if (data2[i].name == "json-to-plain-text") {
+                document.getElementById('j2pt-stars').innerHTML = data2[i].stargazers_count;
+                document.getElementById('j2pt-forks').innerHTML = data2[i].forks;
+                document.getElementById('j2pt-updated-time').innerHTML = fromNow(data2[i].updated_at);
+            }
+            if (data2[i].name == "Tor-Onion-Service-On-Heroku") {
+                document.getElementById('thsoh-stars').innerHTML = data2[i].stargazers_count;
+                document.getElementById('thsoh-forks').innerHTML = data2[i].forks;
+                document.getElementById('thsoh-updated-time').innerHTML = fromNow(data2[i].updated_at);
+            }
+        }
+
+        document.getElementById('total_stargazers_count').innerHTML = total_stars;
+
+    }
+};
+
+http.open('GET', "https://api.github.com/users/sumithemmadi/repos", true);
+http.send();
