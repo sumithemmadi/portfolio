ba486e62fc82757397ca3bf5df52389b9d172e25
sumithemmadi <sumithemmadi244@gmail.com>
2022-04-06 00:06:00 +0530

    Updated

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+
 var i = 0;
 var j = 0;
 var k = 0;
+var l = 0;
+
 var a = 0;
 var data = "";
 
 var command1 = '~$ npm install -g  truecallerjs';
 var command2 = '~$ truecallerjs -s  +919912345678 --name';
+var command3 = '~$ truecallerjs -s  +919912345678 --text';
 
 function cmd1Color(i) {
     var green = [0];
@@ -51,7 +55,7 @@ function cmd2Color(j) {
 function callTruecallerjs() {
     if (i < command1.length) {
         data += "<span style=\"color:" + cmd1Color(i) + ";\">" + command1.charAt(i) + "<span>";
-        document.getElementById("cmd1").innerHTML = data
+        document.getElementById("cmd1").innerHTML = data + '<span class="console-underscore" style="color: white">&#95;</span>'
         i++;
         setTimeout(callTruecallerjs, 100);
     } else if (i == command1.length) {
@@ -93,41 +97,28 @@ function callTruecallerjs() {
         setTimeout(callTruecallerjs, 100);
     } else if (k < command2.length) {
         data += "<span style=\"color:" + cmd2Color(k) + ";\">" + command2.charAt(k) + "<span>";
-        document.getElementById("cmd1").innerHTML = data;
+        document.getElementById("cmd1").innerHTML = data + '<span class="console-underscore" style="color: white">&#95;</span>';
         k++;
         setTimeout(callTruecallerjs, 100);
+    } else if (k == command2.length) {
+        data = '<span style="color: green;">~</span>$ <span style="color: yellow;">npm </span> install <span style="color: gray;">-g </span><span style="color: rgb(18, 223, 238);"> truecallerjs </span><br>changed 25 packages, and audited 26 packages in 5s<br>3 packages are looking for funding<br> run `npm fund` for details<br>found <span style="color: green;">0</span> vulnerabilities<br><span style="color: green;">~</span>$ <span style="color: yellow;"> truecallerjs </span><span style="color: gray;">-s </span> <span style="color: rgb(18, 223, 238);">+919912345678 </span><span style="color: gray;"> --name </span><br><span style = "color: yellow;"> Name </span>: <span style="color: rgb(14, 236, 14);"> Sumith Emmadi</span><br>';
+        document.getElementById("cmd1").innerHTML = data;
+        k++;
+        setTimeout(callTruecallerjs, 1000);
+    } else if (l < command3.length) {
+        data += "<span style=\"color:" + cmd2Color(l) + ";\">" + command3.charAt(l) + "<span>";
+        document.getElementById("cmd1").innerHTML = data + '<span class="console-underscore" style="color: white">&#95;</span>'
+        l++;
+        setTimeout(callTruecallerjs, 100);
+    } else if (l == command3.length) {
+        document.getElementById("cmd1").innerHTML = data + '<br>';
+        l++;
+        setTimeout(callTruecallerjs, 100);
     } else {
-        document.getElementById("cmd1").innerHTML = '<span style="color: green;">~</span>$ <span style="color: yellow;">npm </span> install <span style="color: gray;">-g </span><span style="color: rgb(18, 223, 238);"> truecallerjs </span><br>changed 25 packages, and audited 26 packages in 5s<br>3 packages are looking for funding<br> run `npm fund` for details<br>found <span style="color: green;">0</span> vulnerabilities<br><span style="color: green;">~</span>$ <span style="color: yellow;"> truecallerjs </span> <span style="color: gray;">-s </span> <span style="color: rgb(18, 223, 238);">+919912345678 </span><span style="color: gray;"> --name </span><br><span style = "color: yellow;"> Name </span>: <span style="color: rgb(14, 236, 14);"> Sumith Emmadi</span > ';
+        document.getElementById("cmd1").innerHTML = data + '<table><tr><td style="color: yellow">data</td><td style="color: rgb(18, 223, 238);">:</td></tr><tr><td style="color: yellow">id</td><td style="color: rgb(18, 223, 238);">: jHpw2rckuniunZzGQ/dX0h</td></tr><tr><td style="color: yellow">name</td><td style="color: rgb(18, 223, 238);">: Sumith Emmadi</td></tr><tr><td style="color: yellow">score</td><td style="color: rgb(18, 223, 238);">: 0.35793555</td></tr><tr><td style="color: yellow">access</td><td style="color: rgb(18, 223, 238);">: PUBLIC</td></tr><tr><td style="color: yellow">enhanced</td><td style="color: rgb(18, 223, 238);">: true</td></tr><tr><td style="color: yellow">phones</td><td style="color: rgb(18, 223, 238);">:</td></tr><tr><td style="color: yellow">e164Format</td><td style="color: rgb(18, 223, 238);">: +919912345678</td></tr><tr><td style="color: yellow">numberType</td><td style="color: rgb(18, 223, 238);">: MOBILE</td></tr><tr><td style="color: yellow">nationalFormat</td><td style="color: rgb(18, 223, 238);">: 099123 45678</td></tr><tr><td style="color: yellow">dialingCode</td><td style="color: rgb(18, 223, 238);">: 91</td></tr><tr><td style="color: yellow">countryCode</td><td style="color: rgb(18, 223, 238);">: IN</td></tr><tr><td style="color: yellow">carrier</td><td style="color: rgb(18, 223, 238);">: Vodafone Idea</td></tr><tr><td style="color: yellow">type</td><td style="color: rgb(18, 223, 238);">: openPhone</td></tr><tr><td style="color: yellow">addresses</td><td style="color: rgb(18, 223, 238);">:</td></tr><tr><td style="color: yellow">city</td><td style="color: rgb(18, 223, 238);">: Andhra Pradesh</td></tr><tr><td style="color: yellow">countryCode</td><td style="color: rgb(18, 223, 238);">: IN</td></tr><tr><td style="color: yellow">sourceStats</td><td style="color: rgb(18, 223, 238);">: []</td></tr></table>';
+        l++;
+        setTimeout(callTruecallerjs, 100);
     }
 }
 
-function isOnScreen(elem) {
-    // if the element doesn't exist, abort
-    if (elem.length == 0) {
-        return;
-    }
-    var $window = jQuery(window)
-    var viewport_top = $window.scrollTop()
-    var viewport_height = $window.height()
-    var viewport_bottom = viewport_top + viewport_height
-    var $elem = jQuery(elem)
-    var top = $elem.offset().top
-    var height = $elem.height()
-    var bottom = top + height
-
-    return (top >= viewport_top && top < viewport_bottom) ||
-        (bottom > viewport_top && bottom <= viewport_bottom) ||
-        (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
-}
-
-jQuery(document).ready(function() {
-    window.addEventListener('scroll', function(e) {
-        if (isOnScreen(jQuery('.projects-container'))) {
-            if (a == 0) {
-                a++;
-                setTimeout(callTruecallerjs, 3000);
-            }
-
-        };
-    });
-});
+// json-to-plain-text
