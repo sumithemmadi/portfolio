1e2d9f5dc0a3fcf3c7cef82499f7f4def44536b2
sumithemmadi <sumithemmadi244@gmail.com>
2022-04-06 12:20:40 +0530

    Updated

--- js/j2pt-npm-downloads.js
+++ js/j2pt-npm-downloads.js
@@ -1,12 +1,12 @@
-var url = "https://api.npmjs.org/downloads/point/1970-01-01:2038-01-01/json-to-plain-text";
+var url2 = "https://api.npmjs.org/downloads/point/1970-01-01:2038-01-01/json-to-plain-text";
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
+var xhr5 = new XMLHttpRequest();
+xhr5.open("GET", url2);
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
+xhr5.onreadystatechange = function () {
+    if (xhr5.readyState === 4) {
         document.getElementById("j2pt-downloads").innerHTML = JSON.parse(this.responseText).downloads;
     }
 };
 
-xhr.send();
+xhr5.send();
